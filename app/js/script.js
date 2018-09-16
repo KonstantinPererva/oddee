ab573c2fe662f90976ab0a13feb5d36111efeb22
Pererva <pererva.kostya@outlook.com>
2018-09-17 00:30:51 +0300

    markup section 2 3 4

--- app/js/script.js
+++ app/js/script.js
@@ -2,7 +2,6 @@ var swiper = new Swiper('.page-container', {
     speed: 600,
     direction: 'vertical',
     slidesPerView: 1,
-    freeMode: false,
     resistance: false,
     observer: true,
     mousewheel: {
@@ -12,4 +11,15 @@ var swiper = new Swiper('.page-container', {
     // scrollbar: {
     //     el: '.page-scrollbar',
     // },
+});
+
+var swiperClients = new Swiper('.clients', {
+    speed: 600,
+    slidesPerView: 1
+});
+
+var swiperNews = new Swiper('.news', {
+    speed: 600,
+    slidesPerView: 3,
+    spaceBetween: 6,
 });
