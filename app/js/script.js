d860266e0583ec21673c3ad405b701a80d5a0d17
Pererva <pererva.kostya@outlook.com>
2018-10-15 00:02:59 +0300

    markup section 2,3,4,5

--- app/js/script.js
+++ app/js/script.js
@@ -2,24 +2,50 @@ var swiper = new Swiper('.page-container', {
     speed: 600,
     direction: 'vertical',
     slidesPerView: 1,
-    resistance: false,
-    observer: true,
+    // resistance: false,
+    // observer: true,
     mousewheel: {
         sensitivity: .5
         // forceToAxis: false,
     }
-    // scrollbar: {
-    //     el: '.page-scrollbar',
-    // },
 });
 
 var swiperClients = new Swiper('.clients', {
     speed: 600,
-    slidesPerView: 1
+    slidesPerView: 1,
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
 });
 
 var swiperNews = new Swiper('.news', {
     speed: 600,
     slidesPerView: 3,
     spaceBetween: 6,
+    navigation: {
+        nextEl: '.button-next',
+        prevEl: '.button-prev',
+    },
 });
+
+// document.querySelector('.slider-projects').addEventListener('mouseenter', function () {
+//     swiper.mousewheel.disable();
+// });
+//
+// document.querySelector('.slider-projects').addEventListener('mouseleave', function () {
+//     swiper.mousewheel.enable();
+// });
+//
+// swiper.on('slideChange', function () {
+//     var cont = document.querySelector('.b24-widget-button-position-bottom-right');
+//     if (swiper.activeIndex === 1) {
+//         cont.style.bottom = "130px";
+//     } else {
+//         cont.style.bottom = "30px";
+//     }
+// });
+//
+// swiper.on('resize', function () {
+//     this.init();
+// });
